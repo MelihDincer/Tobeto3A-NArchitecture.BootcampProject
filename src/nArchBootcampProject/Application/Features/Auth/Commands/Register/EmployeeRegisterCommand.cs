b91eaea293d03b59d3ba24665cde82b2657326e9
Melih DİNÇER <m_dincer41@hotmail.com>
2024-04-16 00:45:46 +0300

    User register işlemleri senaryoya uygun olacak bir şekilde revize edildi.

---
+++ src/nArchBootcampProject/Application/Features/Auth/Commands/Register/EmployeeRegisterCommand.cs
@@ -0,0 +1,84 @@
+﻿using Application.Features.Auth.Rules;
+using Application.Services.AuthService;
+using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
+using NArchitecture.Core.Security.Hashing;
+using NArchitecture.Core.Security.JWT;
+
+namespace Application.Features.Auth.Commands.Register;
+
+public class EmployeeRegisterCommand : IRequest<RegisteredResponse>
+{
+    public EmployeeRegisterDto UserForRegisterDto { get; set; }
+    public string IpAddress { get; set; }
+
+    public EmployeeRegisterCommand()
+    {
+        UserForRegisterDto = null!;
+        IpAddress = string.Empty;
+    }
+
+    public EmployeeRegisterCommand(EmployeeRegisterDto userForRegisterDto, string ipAddress)
+    {
+        UserForRegisterDto = userForRegisterDto;
+        IpAddress = ipAddress;
+    }
+
+    public class RegisterCommandHandler : IRequestHandler<EmployeeRegisterCommand, RegisteredResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
+        private readonly AuthBusinessRules _authBusinessRules;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public RegisterCommandHandler(
+            IUserRepository userRepository,
+            IAuthService authService,
+            AuthBusinessRules authBusinessRules,
+            IEmployeeRepository employeeRepository
+        )
+        {
+            _userRepository = userRepository;
+            _authService = authService;
+            _authBusinessRules = authBusinessRules;
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task<RegisteredResponse> Handle(EmployeeRegisterCommand request, CancellationToken cancellationToken)
+        {
+            await _authBusinessRules.UserEmailShouldBeNotExists(request.UserForRegisterDto.Email);
+
+            HashingHelper.CreatePasswordHash(
+                request.UserForRegisterDto.Password,
+                passwordHash: out byte[] passwordHash,
+                passwordSalt: out byte[] passwordSalt
+            );
+            Employee newUser =
+                new()
+                {
+                    FirstName = request.UserForRegisterDto.FirstName,
+                    LastName = request.UserForRegisterDto.LastName,
+                    NationalIdentity = request.UserForRegisterDto.NationalIdentity,
+                    DateOfBirth = request.UserForRegisterDto.DateOfBirth,
+                    Position = request.UserForRegisterDto.Position,
+                    UserName = request.UserForRegisterDto.UserName,
+                    Email = request.UserForRegisterDto.Email,
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt,
+                };
+            Employee createdUser = await _employeeRepository.AddAsync(newUser);
+
+            AccessToken createdAccessToken = await _authService.CreateAccessToken(createdUser);
+
+            Domain.Entities.RefreshToken createdRefreshToken = await _authService.CreateRefreshToken(
+                createdUser,
+                request.IpAddress
+            );
+            Domain.Entities.RefreshToken addedRefreshToken = await _authService.AddRefreshToken(createdRefreshToken);
+
+            RegisteredResponse registeredResponse = new() { AccessToken = createdAccessToken, RefreshToken = addedRefreshToken };
+            return registeredResponse;
+        }
+    }
+}
---
+++ src/nArchBootcampProject/Application/Features/Auth/Commands/Register/InstructorRegisterCommand.cs
@@ -0,0 +1,84 @@
+﻿using Application.Features.Auth.Rules;
+using Application.Services.AuthService;
+using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
+using NArchitecture.Core.Security.Hashing;
+using NArchitecture.Core.Security.JWT;
+
+namespace Application.Features.Auth.Commands.Register;
+
+public class InstructorRegisterCommand : IRequest<RegisteredResponse>
+{
+    public InstructorRegisterDto UserForRegisterDto { get; set; }
+    public string IpAddress { get; set; }
+
+    public InstructorRegisterCommand()
+    {
+        UserForRegisterDto = null!;
+        IpAddress = string.Empty;
+    }
+
+    public InstructorRegisterCommand(InstructorRegisterDto userForRegisterDto, string ipAddress)
+    {
+        UserForRegisterDto = userForRegisterDto;
+        IpAddress = ipAddress;
+    }
+
+    public class RegisterCommandHandler : IRequestHandler<InstructorRegisterCommand, RegisteredResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
+        private readonly AuthBusinessRules _authBusinessRules;
+        private readonly IInstructorRepository _instructorRepository;
+
+        public RegisterCommandHandler(
+            IUserRepository userRepository,
+            IAuthService authService,
+            AuthBusinessRules authBusinessRules,
+            IInstructorRepository ınstructorRepository
+        )
+        {
+            _userRepository = userRepository;
+            _authService = authService;
+            _authBusinessRules = authBusinessRules;
+            _instructorRepository = ınstructorRepository;
+        }
+
+        public async Task<RegisteredResponse> Handle(InstructorRegisterCommand request, CancellationToken cancellationToken)
+        {
+            await _authBusinessRules.UserEmailShouldBeNotExists(request.UserForRegisterDto.Email);
+
+            HashingHelper.CreatePasswordHash(
+                request.UserForRegisterDto.Password,
+                passwordHash: out byte[] passwordHash,
+                passwordSalt: out byte[] passwordSalt
+            );
+            Instructor newUser =
+                new()
+                {
+                    FirstName = request.UserForRegisterDto.FirstName,
+                    LastName = request.UserForRegisterDto.LastName,
+                    NationalIdentity = request.UserForRegisterDto.NationalIdentity,
+                    DateOfBirth = request.UserForRegisterDto.DateOfBirth,
+                    CompanyName = request.UserForRegisterDto.CompanyName,
+                    UserName = request.UserForRegisterDto.UserName,
+                    Email = request.UserForRegisterDto.Email,
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt,
+                };
+            Instructor createdUser = await _instructorRepository.AddAsync(newUser);
+
+            AccessToken createdAccessToken = await _authService.CreateAccessToken(createdUser);
+
+            Domain.Entities.RefreshToken createdRefreshToken = await _authService.CreateRefreshToken(
+                createdUser,
+                request.IpAddress
+            );
+            Domain.Entities.RefreshToken addedRefreshToken = await _authService.AddRefreshToken(createdRefreshToken);
+
+            RegisteredResponse registeredResponse = new() { AccessToken = createdAccessToken, RefreshToken = addedRefreshToken };
+            return registeredResponse;
+        }
+    }
+}
